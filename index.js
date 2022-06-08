e6a938610a41eed3c1ed867a290012f48cc635b3
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2022-06-08 15:26:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter')
+const promoRouter = require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
+
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+
+app.use('/promotions', promoRouter)
+
+app.use('/dishes', dishRouter)
+
+app.use('/leaders', leaderRouter)
+
+
+app.use(express.static(__dirname + '/public'))
+
+app.use((req,res,next)=> {
+    console.log(req.header);
+    res.statusCode = 200;
+    res.setHeader('COntext-Type', 'text/html');
+    res.end('<html><body><h1>This is an express server </h1></body></html>')
+})
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, ()=> {
+    console.log('server running')
+})
