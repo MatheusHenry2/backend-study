34dd6ce4232abb09785722593abf0085839befaf
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2022-06-15 14:47:19 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter')
+var leaderRouter = require('./routes/leaderRouter')
+var promoRouter = require('./routes/promoRouter')
+
+const mongoose = require('mongoose');
+
+const dishes = require('./models/dishes')
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+
+connect.then((db)=>{
+  console.log('COnnected correctly server');
+}, (err)=>{
+  console.log(err)
+});
+
 
 var app = express();
 
@@ -21,6 +39,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter)
+app.use('/promotions', promoRouter)
+app.use('/leaders', leaderRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
